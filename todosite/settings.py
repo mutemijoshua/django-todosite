5d16479c94bed17c2c2539a67bb29cf540645693
mutemijoshua <joshuamutua555@gmail.com>
2024-03-31 14:21:43 +0300

    requirements

--- todosite/settings.py
+++ todosite/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 import sys
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -146,3 +146,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'todosite/static')]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
